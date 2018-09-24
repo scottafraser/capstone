9b20754e94468be8e6efeb054b00b416ad07ca4e
Student <example@example.com>
2018-09-24 14:49:37 -0700

    checking

---
+++ client/src/store/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "../reducers";
+
+export default function configureStore(intialState) {
+  return createStore(rootReducer, intialState, applyMiddleware(thunk));
+}
