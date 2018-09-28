6cb1c31a7379c00a1a889f2283fcab861962823a
Scott Fraser <scottafraser@yahoo.com>
2018-09-28 16:41:13 -0700

    need to try and get my form inputs to set to the redux store

--- client/src/reducers/genreReducer.js
+++ client/src/reducers/genreReducer.js
@@ -3,6 +3,10 @@ export function setGenre(state = "", action) {
     case "SET_GENRE":
       console.log(action.setGenre);
       return action.setGenre;
+
+    case "GET_GENRE":
+      return action.setGenre;
+
     default:
       return state;
   }
